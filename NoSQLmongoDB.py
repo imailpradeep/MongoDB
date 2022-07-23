f3e4fd5b4e5eb90967cc9752229facf548dbd21d
imailpradeep <imailpradeep1@gmail.com>
2022-07-23 15:52:10 +0530

    NoSQLMongoDB practice file

--- NoSQLmongoDB.py
+++ NoSQLmongoDB.py
@@ -1,1 +1,13 @@
 # file for practicing MongoDB no SQL database
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://imailpradeep:ammaacha@cluster0.gujy4jv.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+data = {
+    "fname" : "Pradeep",
+    "lname" : "Bhaskar",
+    "email" : "imailpradeep1@gmail.com",
+    "subject" : ["data science", "big data", "data analytics"]
+}
